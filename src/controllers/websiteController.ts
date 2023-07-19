b5cefa187a088f0f7cb3ceadd67868d2252959c8
sirguy12 <ryankairo09@yahoo.com.br>
2023-07-18 21:59:45 -0300

    feat: added /view/:id to see image preview in some platforms

--- src/controllers/websiteController.ts
+++ src/controllers/websiteController.ts
@@ -1,15 +1,32 @@
 import {Request,Response } from "express"
-
+import S3storage from "../utils/S3storage";
 
 const indexPage = (req:Request, res: Response)=>{
     res.render('index')
 }
 
-const seeFilePage = (req:Request, res: Response)=>{
+const getImagePipe = (req:Request, res: Response) =>{
+    const {id} = req.params
+    if(id){
+      const aws_resposne = S3storage.getFileStream(id)
+  
+      aws_resposne.on('error', (err: any) => {
+        if (err.code === 'AccessDenied') {
+          return res.status(403).json({ error: 'Acesso negado ao acessar o arquivo' });
+        }
+        return res.status(500).json({ error: 'Erro ao acessar o arquivo' });
+      });
+      return aws_resposne.pipe(res)
+      
+    }
+    else{
+      return res.status(500).json({ error: 'Parâmetro id não foi informado' });
+    }
+  }
+const seeImagePreview = (req:Request, res: Response) =>{
     const id = req.params
-    res.json({message:'response'})
+    res.render('preview',{id})
 }
 
 
-
-export {indexPage,seeFilePage}
+export {indexPage,getImagePipe,seeImagePreview}
