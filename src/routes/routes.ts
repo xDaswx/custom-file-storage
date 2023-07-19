ed0445afd7ddbf6331b9c6d1062ebabb200137f9
sirguy12 <ryankairo09@yahoo.com.br>
2023-07-18 21:35:04 -0300

    moved: getImagePipe to websiteController

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,16 @@
 import { Router,Request,Response } from "express"
-import { indexPage, seeFilePage } from "../controllers/websiteController";
-import { uploadImagev2,getImagePipe,removeImage } from "../controllers/apiController";
+import { indexPage,getImagePipe } from "../controllers/websiteController";
+import { uploadImagev2,removeImage } from "../controllers/apiController";
 import multerHandler from "../multer/multerValidator"
 
 const route = Router()
 
 //websiteController
 route.get('/',indexPage)
+route.get('/media/:id',getImagePipe)
 //apiController
 route.post('/upload/',multerHandler ,uploadImagev2)
-route.get('/media/:id',getImagePipe)
+
 route.get('/remove/:id',removeImage)
 
 export default route;
